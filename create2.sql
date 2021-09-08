dac513db272a3172171fd26988f76e09228b14e2
johnnymeneses <bilbo_vader@outlook.com>
2021-09-08 13:51:07 -0300

    Adicionado novos arquivos

---
+++ create2.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE [dbo].[estudos]
+(
+	[dia] [datetime] NOT NULL,
+	[Objetivo] [varchar](30) NOT NULL
+) 
+
+
+INSERT	INTO		ESTUDOS
+VALUES 
+		--('2021-07-03','CRIAÇÃO DE TABELA SQL')
+		--('','')
+
+
+SELECT	*
+FROM	ESTUDOS
+
